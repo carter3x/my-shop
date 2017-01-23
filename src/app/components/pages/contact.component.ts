3932addcfa0f32144d2560444071103314d7d112
Avalon <valentyl4gf@gmail.com>
2017-01-23 14:19:12 -0800

    new gh-pages version

--- src/app/components/pages/contact.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  moduleId:module.id,
-  selector: 'app-contact',
-  templateUrl: 'contact.component.html',
-})
-
-export class ContactComponent  {}
